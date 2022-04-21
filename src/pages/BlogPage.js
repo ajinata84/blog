fd1b582f8fb9fb7ccb5084114b9ceda0222e5515
ajinata84 <ajinata89@gmail.com>
2022-04-21 14:21:31 +0700

    added search bar

--- src/pages/BlogPage.js
+++ src/pages/BlogPage.js
@@ -3,6 +3,7 @@ import { Container, Text, Spinner, Center } from "@chakra-ui/react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import { baseUrl } from "../config";
+import ScrollToTop from "../components/ScrollToTop";
 
 import { motion } from "framer-motion";
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
@@ -37,66 +38,69 @@ export default function BlogPage() {
   if (loading) {
     return (
       <Center height={"100vh"}>
-        <Spinner color="orange" />
+        <Spinner color="orange" position={"fixed"} />
       </Center>
     );
   } else {
     return (
-      <motion.div
-        initial={{ y: 80, opacity: 0 }}
-        animate={{
-          y: 0,
-          opacity: 1,
-          ...transition,
-        }}
-        exit={{ y: 300, opacity: 0, ...transition }}
-      >
-        <BlogTitle
-          title={apiData.title}
-          author={apiData.author}
-          date={apiData.date}
-        />
-        <MyStyle>
-          <Container
-            p={{ base: "0" }}
-            mb="100px"
-            className="markdown-body"
-            flex={{ base: undefined, md: 1 }}
-            maxW={{ base: undefined, md: "100%" }}
-          >
-            <ReactMarkdown
-              children={post}
-              remarkPlugins={remarkGfm}
-              rehypePlugins={rehypeRaw}
-              components={{
-                code({ node, inline, className, children, ...props }) {
-                  const match = /language-(\w+)/.exec(className || "");
-                  return !inline && match ? (
-                    <SyntaxHighlighter
-                      children={String(children).replace(/\n$/, "")}
-                      style={oneDark}
-                      language={match[1]}
-                      PreTag="div"
-                      {...props}
-                    />
-                  ) : (
-                    <code className={className} {...props}>
-                      {children}
-                    </code>
-                  );
-                },
-              }}
-            />
-          </Container>
-        </MyStyle>
-      </motion.div>
+      <>
+        <ScrollToTop />
+        <motion.div
+          initial={{ y: 80, opacity: 0 }}
+          animate={{
+            y: 0,
+            opacity: 1,
+            ...transition,
+          }}
+          exit={{ y: 300, opacity: 0, ...transition }}
+        >
+          <BlogTitle
+            title={apiData.title}
+            author={apiData.author}
+            date={apiData.date}
+          />
+          <MyStyle>
+            <Container
+              p={{ base: "0" }}
+              mb="100px"
+              className="markdown-body"
+              flex={{ base: undefined, md: 1 }}
+              maxW={{ base: undefined, md: "100%" }}
+            >
+              <ReactMarkdown
+                children={post}
+                remarkPlugins={remarkGfm}
+                rehypePlugins={rehypeRaw}
+                components={{
+                  code({ node, inline, className, children, ...props }) {
+                    const match = /language-(\w+)/.exec(className || "");
+                    return !inline && match ? (
+                      <SyntaxHighlighter
+                        children={String(children).replace(/\n$/, "")}
+                        style={oneDark}
+                        language={match[1]}
+                        PreTag="div"
+                        {...props}
+                      />
+                    ) : (
+                      <code className={className} {...props}>
+                        {children}
+                      </code>
+                    );
+                  },
+                }}
+              />
+            </Container>
+          </MyStyle>
+        </motion.div>
+      </>
     );
   }
 }
 
 const BlogTitle = ({ title = "title", author = "author", date = "date" }) => {
   return (
-    <div style={{ marginBottom: "10px", textAlign: "center" }}>
+    <div style={{ marginBottom: "30px", textAlign: "center" }}>
       <h1 className="blog-title">{title}</h1>
 
       <Text textAlign={"center"}>{`${author}, ${date}`}</Text>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,36 +2,88 @@ import React, { useState, useEffect } from "react";
 import PageName from "../components/PageName";
 import SelectionCard from "../components/SelectionCard";
 import axios from "axios";
+import { motion } from "framer-motion";
 import { baseUrl } from "../config";
-import { Center, Spinner } from "@chakra-ui/react";
+import {
+  Center,
+  Spinner,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputLeftElement,
+} from "@chakra-ui/react";
+
+import { SearchIcon } from "@chakra-ui/icons";
+
+const transition = { transition: { duration: 0.3, type: "linear" } };
 
 export default function Home() {
   const url = `${baseUrl}`;
   const [postsData, setPostsData] = useState([]);
+  const [permData, setPermData] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [searchText, setSearchText] = useState("");
 
   useEffect(() => {
     axios
       .get(url)
       .then((res) => {
         setPostsData(res.data.data);
+        setPermData(res.data.data);
         setLoading(false);
       })
       .catch((err) => console.log(err));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  useEffect(() => {
+    if (searchText === "") setPostsData(permData);
+    setPostsData(() =>
+      permData.filter((item) =>
+        item.data.title.toLowerCase().match(searchText.toLowerCase())
+      )
+    );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [searchText]);
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setSearchText(e.target.value);
+    if (!e.target.value.length > 0) {
+      setPostsData(postsData);
+    }
+    console.log(postsData);
+  };
 
   if (loading) {
     return (
       <Center height={"100vh"}>
-        <Spinner color="orange" />
+        <Spinner color="orange" position={"fixed"} />
       </Center>
     );
   } else {
     return (
       <div>
         <PageName />
+        <motion.div
+          initial={{ y: 80, opacity: 0 }}
+          animate={{
+            y: 0,
+            opacity: 1,
+            ...transition,
+          }}
+          exit={{ y: 80, opacity: 0, ...transition }}
+        >
+          <FormControl maxW={{ base: "100%", md: "60%" }} paddingX="16px">
+            <FormLabel>Search</FormLabel>
+            <InputGroup>
+              <InputLeftElement children={<SearchIcon />} />
+              <Input onChange={handleChange} value={searchText} />
+            </InputGroup>
+          </FormControl>
+        </motion.div>
         {postsData.map((data) => {
-          console.log(data);
           return (
             <SelectionCard
               id={data.ref["@ref"].id}
